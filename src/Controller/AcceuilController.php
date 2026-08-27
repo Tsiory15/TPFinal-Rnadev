6ce263694c59fa1c21afee2f47f9e6c3c506df1b
Tsiory <raphaeltsiory15@gmail.com>
2026-08-27 08:22:18 +0300

    fix bug

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AcceuilController extends AbstractController
 {
-    #[Route('/', name: 'app_acceuil')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('acceuil/index.html.twig', [
