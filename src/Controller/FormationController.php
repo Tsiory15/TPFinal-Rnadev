432ac820fd42ebae85104de64c219f46d5d247ea
Tsiory <raphaeltsiory15@gmail.com>
2026-09-01 09:21:41 +0300

    redesign pagination rechercher

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -5,27 +5,35 @@ namespace App\Controller;
 use App\Repository\CategorieRepository;
 use App\Repository\ModuleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FormationController extends AbstractController
 {
-    private ModuleRepository $mr;
-    private CategorieRepository $cr;
-    public function __construct(ModuleRepository $mr, CategorieRepository $cr)
+    private ModuleRepository $ModuleRepository;
+    private CategorieRepository $CategorieRepository;
+    public function __construct(ModuleRepository $ModuleRepository, CategorieRepository $CategorieRepository)
     {
-        $this->mr = $mr;
-        $this->cr = $cr;
+        $this->ModuleRepository = $ModuleRepository;
+        $this->CategorieRepository = $CategorieRepository;
     }
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $listCateg = $this->cr->getCateg();
-        $listModule = $this->mr->getModule();
+        $Mot = $request->request->get('search','');
+        $listCateg = $this->CategorieRepository->getCateg();
+        $limit = 5;
+        $page = $request->query->getInt('page','1');
+        $listModule = $this->ModuleRepository->getModule($Mot,$page,$limit);
+        $totalItem = count($listModule);
+        $totalPage = ceil($totalItem/$limit);
         return $this->render('formation/index.html.twig', [
             'controller_name' => 'FormationController',
             'listModule'=> $listModule,
-            'listCateg' => $listCateg
+            'listCateg' => $listCateg,
+            'totalItem' => $totalPage,
+            'CurrentPage' => $page
         ]);
     }
 }
